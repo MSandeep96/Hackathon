723602d85df4b61c2e68a497213dc069fc450d6c
Sandeep Reddy <msande1996@gmail.com>
2018-06-03 16:04:30 +0530

    Add api for bulk insert

--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -14,6 +14,16 @@ router.post('/', function(req, res, next) {
   .catch(next);
 });
 
+router.post('/dummy',function(req,res,next){
+  User.insertMany(req.body)
+    .then(function(doc){
+      res.sendStatus(200);
+    })
+    .catch(function(err){
+      res.status(400).send(err);
+    });
+});
+
 router.use(function(req,res){
   var user = req.body;
   user.genToken().then((token) => {
