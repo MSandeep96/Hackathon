4bb822a0bfa5fb27e0b06f4e1247f61b537a19ff
Sandeep Reddy <msande1996@gmail.com>
2018-06-03 13:46:33 +0530

    Add api for game solving

--- BackEnd/routes/index.js
+++ BackEnd/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
+var solver = require('../algoutil/solveboard');
+var pick = require('loadash.pick');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -11,5 +14,17 @@ router.get('/leaderboard', function(req,res,next) {
     res.send(leaderboard);
   });
 });
+
+router.post('/solve', function(req,res,next){
+  solver.solveBoard(req.body.config)
+  .then((moves,timeTaken)=>{
+    var answer = {
+      moves,
+      timeTaken
+    };
+    res.send(answer);
+  })
+})
+
 
 module.exports = router;
